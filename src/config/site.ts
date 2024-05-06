c3e313fe7eb7b5e170d8693e3a07ec41ac2f3ca1
deaving-71 <110570275+deaving-71@users.noreply.github.com>
2024-05-06 21:07:57 +0100

    update metadata

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,11 +2,12 @@ import { SiteConfig } from "@/types"
 
 import { env } from "@/env.mjs"
 
+import { About } from "./about"
+
 export const siteConfig: SiteConfig = {
   name: "DeaViNG",
   author: "deaving-71",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+  description: About.about_me,
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
